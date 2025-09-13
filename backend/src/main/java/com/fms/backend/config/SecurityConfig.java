5567d9f4fd9158066087fb8a6cb646974cc379f2
felipe.simoes <felsimoes@gmail.com>
2025-09-12 21:26:49 -0300

    feat(auth): configura security e endpoint de login

--- backend/src/main/java/com/fms/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/fms/backend/config/SecurityConfig.java
@@ -1,17 +1,52 @@
 package com.fms.backend.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import static com.fms.backend.utils.KeyUtils.getPrivateKeyFromString;
+import static com.fms.backend.utils.KeyUtils.getPublicKeyFromString;
+
 @Configuration
-@EnableMethodSecurity
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Value("${jwt.private.key}")
+    private String privateKeyPath;
+
+    @Value("${jwt.public.key}")
+    private String publicKeyPath;
+
+    @Bean
+    public RSAPublicKey publicKey() throws Exception {
+        String key = Files.readString(Paths.get(publicKeyPath));
+        return (RSAPublicKey) getPublicKeyFromString(key);
+    }
+
+    @Bean
+    public RSAPrivateKey privateKey() throws Exception {
+        String key = Files.readString(Paths.get(privateKeyPath));
+        return (RSAPrivateKey) getPrivateKeyFromString(key);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,5 +64,12 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() throws Exception {
+        JWK jwk = new RSAKey.Builder(publicKey()).privateKey(privateKey()).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
--- backend/src/main/java/com/fms/backend/controllers/auth/AuthController.java
+++ backend/src/main/java/com/fms/backend/controllers/auth/AuthController.java
@@ -1,16 +1,26 @@
 package com.fms.backend.controllers.auth;
 
+import com.fms.backend.dto.auth.LoginRequestDTO;
+import com.fms.backend.dto.auth.LoginResponseDTO;
+import com.fms.backend.services.auth.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/login")
-    public ResponseEntity<String> login() {
-        return ResponseEntity.ok("Login endpoint");
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        return authService.login(loginRequestDTO);
     }
 }
