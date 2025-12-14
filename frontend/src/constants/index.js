2a4baac2a02ab3adf8597b2a3c39f2f240b4d043
felipe.simoes <felsimoes@gmail.com>
2025-12-14 14:12:46 -0300

    feat(frontend): criação do section Hero e componente botão

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,34 @@
+const navLinks = [
+    {
+        name: "Projetos",
+        link: "#work",
+    },
+    {
+        name: "Experiência",
+        link: "#experience",
+    },
+    {
+        name: "Habilidades",
+        link: "#skills",
+    },
+    {
+        name: "Depoimentos",
+        link: "#testimonials",
+    },
+];
+
+const words = [
+    { text: 'Ideias', imgPath: '/images/ideas.svg' },
+    { text: 'Conceitos', imgPath: '/images/concepts.svg' },
+    { text: 'Designs', imgPath: '/images/designs.svg' },
+    { text: 'Código', imgPath: '/images/code.svg' },
+    { text: 'Ideias', imgPath: '/images/ideas.svg' },
+    { text: 'Conceitos', imgPath: '/images/concepts.svg' },
+    { text: 'Designs', imgPath: '/images/designs.svg' },
+    { text: 'Código', imgPath: '/images/code.svg' },
+];
+
+export {
+  words,
+  navLinks,
+};
